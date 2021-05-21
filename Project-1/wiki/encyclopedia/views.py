5f0ff4deaaaff212550661d78f34e2e6e62ed2f2
AsifRashid01 <asif.rashid20@gmail.com>
2021-05-21 18:32:14 -0400

    implement entry page

--- Project-1/wiki/encyclopedia/views.py
+++ Project-1/wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    # retrieve entry using util
+    content = util.get_entry(title)
+
+    if content == None:
+        return render(request, "encyclopedia/noentry.html")
+
+    content_html = markdown2.markdown(content)
+
+    return render(request, "encyclopedia/entry.html", {
+        "content": content_html,
+        "title": title
+    })
