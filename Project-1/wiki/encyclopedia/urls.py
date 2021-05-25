da9ee3aec3955846ae04de1a0852ed9839638010
AsifRashid01 <asif.rashid20@gmail.com>
2021-05-24 20:15:40 -0400

    add search functionality

--- Project-1/wiki/encyclopedia/urls.py
+++ Project-1/wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("wiki/<title>", views.entry, name="entry"),
+    path("search", views.search, name="search")
 ]
--- Project-1/wiki/encyclopedia/views.py
+++ Project-1/wiki/encyclopedia/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from django import forms
+from django.http import HttpResponseRedirect, HttpResponse
 
 from . import util
 import markdown2
+
+#class NewTaskForm(forms.Form):
+ #   task = forms.CharField(label="Entry Query")
 
 
 def index(request):
@@ -10,7 +15,6 @@ def index(request):
     })
 
 def entry(request, title):
-    # retrieve entry using util
     content = util.get_entry(title)
 
     if content == None:
@@ -22,3 +26,16 @@ def entry(request, title):
         "content": content_html,
         "title": title
     })
+
+def search(request):
+
+    q = request.GET.get('q').strip()
+    if q in util.list_entries():
+        return redirect("entry", title=q)
+
+    related_entries = [entry for entry in util.list_entries() if q.lower() in entry.lower()]
+
+    if related_entries == []:
+        return render(request, "encyclopedia/noentry.html")
+
+    return render(request, "encyclopedia/search.html", {"entries": related_entries, "q": q})
